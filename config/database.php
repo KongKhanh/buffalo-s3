aa521ff9b0c6feb42981f58418467fc1d4b04fbe
Hau <hauvo1709@gmail.com>
2022-02-28 17:52:21 +0700

    update config database

--- config/database.php
+++ config/database.php
@@ -8,7 +8,7 @@
                 'driver' => 'mysql',
                 'host' => '127.0.0.1',
                 'port' => '3306',
-                'database' => 's3',
+                'database' => 'ats_edu_vn',
                 'username' => 'root',
                 'password' => '',
                 'charset' => 'utf8mb4',
