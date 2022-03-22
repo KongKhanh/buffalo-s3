efc6e461cba5ca46d8bacabfa5eb4fd09600b254
Hau <hauvo1709@gmail.com>
2022-03-23 01:27:48 +0700

    build ClientPage

--- resources/views/layouts/dashboard/app.dashboard.view.php
+++ resources/views/layouts/dashboard/app.dashboard.view.php
@@ -47,13 +47,13 @@
                 <div class="container-fluid">
                     <div class="row">
                         <div class="col-md-6">
-                            <script>document.write(new Date().getFullYear())</script> © Hyper - Coderthemes.com
+                            <script>document.write(new Date().getFullYear())</script> - https://www.ats.edu.vn
                         </div>
                         <div class="col-md-6">
                             <div class="text-md-end footer-links d-none d-md-block">
-                                <a href="javascript: void(0);">About</a>
+                                <!-- <a href="javascript: void(0);">About</a>
                                 <a href="javascript: void(0);">Support</a>
-                                <a href="javascript: void(0);">Contact Us</a>
+                                <a href="javascript: void(0);">Contact Us</a> -->
                             </div>
                         </div>
                     </div>
--- resources/views/pages/client/major_review.view.php
+++ resources/views/pages/client/major_review.view.php
@@ -27,49 +27,28 @@
             <!--Navbar Header-->
             <?php includeFile('pages/client/components/navbar_header.view.php');?>
 
-            <section class="container main-header__desc">
+            <section class="container main-header__desc main-header__desc-no-home">
                 <h1 class="desc__title" style="text-align: center; text-transform: uppercase;" id="page-title">
                     Giới thiệu ngành <?= $MajorItem['mjr_name']; ?>
                 </h1>
-                <nav class="history-bar">
+                <!-- <nav class="history-bar">
                     <p>
                         <a href="index.html" class="previous-page">Trang chủ </a>
                         <a href="#" class="previous-page">Đào tạo </a>
-                        <a href="#" class="present-page"> <?= $MajorItem['mjr_name']; ?></a>
+                        <a href="#" class="present-page">  -->
+                            <?= 
+                                "";
+                                // $MajorItem['mjr_name']; 
+                            ?>
+                        <!-- </a>
                     </p>
-                </nav>
+                </nav> -->
             </section>
         </header>
-        <!-- #region News -->
-        <nav class="navbar__mobile">
-            <label for="nav-mobile-input" class="nav__bars-btn">
-                <i class="fas fa-bars"></i>
-            </label>
-            <input type="checkbox" name="" class="nav__input" id="nav-mobile-input" hidden>
-            <label for="nav-mobile-input" class="nav-overlay"></label>
-            <ul class="menubar__list nav__mobile">
-                <label for="nav-mobile-input"  class="nav__mobile-close"><i class="fas fa-times"></i></label>
-                <li class="menubar__mobile-list-item"><img src="/public/client/assets/images/ats-logo-2.png" alt=""></li>
-                <li class="menubar__mobile-list-item" id="visited"><a href="#">Trang chủ</a></li>
-                <li class="menubar__mobile-list-item"><a href="#">Giới thiệu</a></li>
-                <li class="menubar__mobile-list-item"><a href="#">Phòng ban</a></li>
-                <li class="menubar__mobile-list-item"><a href="#">Tin tức</a></li>
-                <li class="menubar__mobile-list-item"><a href="#">Liên hệ</a></li>
-                <li class="menubar__mobile-list-item"><a href="#">Tuyển dụng</a></li>
-                <li class="menubar__list-item">
-                    <input type="text" style="
-                    padding: .25em;
-                    margin: 0 .25em;
-                    border-radius: .5em;
-                    border: none;
-                    width: 80%;" placeholder="Tìm kiếm">
-                    <button name="header-search--btn" class="btn" style="color: white">
-                        <i class="fas fa-search"></i>
-                    </button>
-                </li>
-            </ul>
-        </nav>
-        <!-- #endregion News -->
+
+        <!--Navbar Header-->
+        <?php includeFile('pages/client/components/navbar_header-mobile.view.php');?>
+
         <section class="news-container container">
             <section class="news-block">
                 <article class="news-box no-flex">
