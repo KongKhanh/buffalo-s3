cba27914fd2d0f5048592b8e0606510a9139fa76
Khanh <nguyenckhanh423@gmail.com>
2022-03-25 13:41:46 +0700

    Second Menu Mobile UI

--- resources/views/pages/client/components/navbar_header-mobile.view.php
+++ resources/views/pages/client/components/navbar_header-mobile.view.php
@@ -5,6 +5,7 @@
 ?>
 
 <style>
+    
     .sub-menu-a {
 
     }
@@ -34,7 +35,7 @@
 
                     $subCateA .= <<<HTML
 
-                        <li><a href="{$subCate['link_url']}" style="text-decoration: none;">- {$subCate['mc_title']}</a></li>
+                        <li><a class="get-menu-id" data-id="{$subCate['mc_parent_id']}" href="{$subCate['link_url']}" style="text-decoration: none;">- {$subCate['mc_title']}</a></li>
 
                     HTML;
                 }
@@ -43,7 +44,7 @@
                 <<<HTML
 
                     <li class="menubar__mobile-list-item">
-                        <a href="{$menuCategories[$i]['link_url']}">{$menuCategories[$i]['mc_title']}<i class="fas fa-caret-right" style="margin-left: 6px;"></i></a>
+                        <a class="onclick-submenu" data-id="{$menuCategories[$i]['mc_id']}">{$menuCategories[$i]['mc_title']}<i class="fas fa-caret-right" style="margin-left: 6px;"></i></a>
                             <ul class="sub-menu-a">
                                 {$subCateA}
                             </ul>
@@ -72,3 +73,20 @@
         </li> -->
     </ul>
 </nav>
+<script>
+    $('.sub-menu-a').css('display', 'none');
+
+    var subMenuId = ($('.get-menu-id').attr('data-id'));
+
+
+    $('.onclick-submenu').click(function(){
+      
+        var MenuId = ($(this).attr('data-id'));
+        if(MenuId == subMenuId){
+            $('.sub-menu-a').css('display', 'block');
+    }
+    else{
+        $('.sub-menu-a').css('display', 'none');
+    }
+    })  
+</script>
